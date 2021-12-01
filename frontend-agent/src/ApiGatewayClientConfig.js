88a483e0c6b8cd75bcc2dff535f525d0861bca89
Tom Burge <tomburge@amazon.com>
2021-12-01 21:44:30 +0000

    my branch changes

--- frontend-agent/src/ApiGatewayClientConfig.js
+++ frontend-agent/src/ApiGatewayClientConfig.js
@@ -2,7 +2,7 @@
 import { INVOKE_URL, ACCESS_KEY, SECRET_KEY, AWS_REGION } from './AgentConfig';
 
 const APIGatewayClientConfig = {
-  invokeUrl: INVOKE_URL,
+  invokeUrl: "https://oyyrrrp4sb.execute-api.us-east-1.amazonaws.com/prod/",
   accessKey: ACCESS_KEY,
   secretKey: SECRET_KEY,
   region: AWS_REGION
