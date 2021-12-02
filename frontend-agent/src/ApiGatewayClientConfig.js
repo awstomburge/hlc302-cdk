351e1f553be920d457a18f67cf73c9dbe6da6d30
Jonathan Schellack <schellac@amazon.com>
2021-12-02 08:26:55 -0800

    Revert "Update ApiGatewayClientConfig.js"
    This reverts commit 3b06b9d23044bbc73c1348137187d2c1614cab42.

--- frontend-agent/src/ApiGatewayClientConfig.js
+++ frontend-agent/src/ApiGatewayClientConfig.js
@@ -2,7 +2,7 @@
 import { INVOKE_URL, ACCESS_KEY, SECRET_KEY, AWS_REGION } from './AgentConfig';
 
 const APIGatewayClientConfig = {
-  invokeUrl: "https://au6huhcl0m.execute-api.us-east-1.amazonaws.com/prod/",
+  invokeUrl: INVOKE_URL,
   accessKey: ACCESS_KEY,
   secretKey: SECRET_KEY,
   region: AWS_REGION
